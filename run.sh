2143708a83d1e88951c2c8d7da4810fea77759d8
Bram Mittendorff <b.mittendorff@nerds.company>
2016-02-17 20:19:00 +0100

    You can change your project name in the docker-compose file now

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
-PROJECT_NAME='mywebsite'
+PROJECT_NAME="$1"
 
-if [ ! -d "$PROJECT_NAME" ]; then
-  wagtail start $PROJECT_NAME
+if [ "$PROJECT_NAME" ]; then
+  echo "$PROJECT_NAME/*" >> .gitignore
+
+  if [ ! -d "$PROJECT_NAME" ]; then
+    wagtail start $PROJECT_NAME
+  fi
+
+  cd $PROJECT_NAME
+
+  python manage.py migrate
+  python manage.py update_index
+  python manage.py runserver 0.0.0.0:8000
+else
+  echo "Usage: run.sh <myproject>"
 fi
-cd $PROJECT_NAME
-
-python manage.py migrate
-python manage.py update_index
-python manage.py runserver 0.0.0.0:8000
